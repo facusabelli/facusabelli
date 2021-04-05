77213312eee175ed130decfb7114c3603858925d
facusabelli <73084328+facusabelli@users.noreply.github.com>
2021-04-05 17:01:39 -0300

    Add files via upload

---
+++ taller/main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Main {
+
+public static void main(String[] args) {
+        Persona p1 = new Persona();
+        Persona p2 = new Persona("jose");
+        Persona p3 = new Persona("pablo",2,2345678,23456789,"Triunvirato 2345");
+        
+
+
+
+
+
+
+
+/*public class Main {
+
+
+    public static void main(String[] args) {
+        Persona p = new Persona();
+
+        String n = p.getNombre();
+        System.out.println(n);
+        p.setNombre("pablo");
+        n= p.getNombre();
+        System.out.println(n);
+
+        int e= p.getEdad();
+        System.out.println(e);
+        p.setEdad(10);
+        e= p.getEdad();
+        System.out.println(e);
+
+
+    }
+}
