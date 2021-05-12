721e1fb6cc81d2a032e9dd32eb01bcf4d9f9b3f4
facusabelli <73084328+facusabelli@users.noreply.github.com>
2021-05-12 10:43:10 -0300

    Add files via upload

---
+++ laboratorio/main9.5.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        SistemaRegistro a = new SistemaRegistro();
+        a.agregagarNumero();
+        a.agregagarOtroNumero();
+        a.mismoNumero();
+    }
+}
