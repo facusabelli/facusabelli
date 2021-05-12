9727e6bf166446a8f8d4dbee15d9b9ff50b2aed3
facusabelli <73084328+facusabelli@users.noreply.github.com>
2021-05-12 10:41:56 -0300

    Delete main3.5.java

--- laboratorio/main3.5.java
+++
@@ -1,8 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        SistemaRegistro a = new SistemaRegistro();
-        a.agregagarNumero();
-        a.agregagarOtroNumero();
-        a.mismoNumero();
-    }
-}
