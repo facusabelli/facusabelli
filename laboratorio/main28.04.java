69da5ec9045700427d15ea6dc1fa13ff1240713d
facusabelli <73084328+facusabelli@users.noreply.github.com>
2021-05-11 22:30:53 -0300

    Add files via upload

---
+++ laboratorio/main28.04.java
@@ -0,0 +1,10 @@
+package com.company;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args){
+    System.out.println("Bienvenido al sistema");
+    Scanner apa = new Scanner(System.in);
+    System.out.println("Que quiere hacer: 1- Alta persona,2-Baja Persona,3-Modificar Persona,4-Personas +18,5- Mostrar todas las personas,6-Salirs del sistema");
+    String eleccion 
+}
