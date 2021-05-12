0cf5121e6f576aa9630bfe8f04eeb600f1c6e975
facusabelli <73084328+facusabelli@users.noreply.github.com>
2021-05-12 10:48:56 -0300

    Add files via upload

---
+++ laboratorio/SistemaDeRegistroDeNumeros9.5.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class SistemaRegistroDeNumeros{
+
+    private HashSet<Integer> HashNumero1;
+    private HashSet<Integer> HashNumero2;
+    
+    public SistemaRegistro(){
+        this.HashNumero1 = new HashSet<>();
+        this.HashNumero2 = new HashSet<>();
+    }
+
+    public void agregagarNumero(){
+        Scanner numeros = new Scanner(System.in);
+        System.out.println("¿Cuantos numeros quiere agregar?");
+        float preguntaPersonas = numeros.nextFloat();
+        for (int i = 0; i < preguntaPersonas; i++) {
+            Scanner nuevoNumero = new Scanner(System.in);
+            System.out.println("¿Que numero quiere agregar?");
+            int numero = nuevoNumero.nextInt();
+            this.HashNumero1.add(numero);
+        }
+    }
+
+    public void agregagarOtroNumero(){
+        Scanner numeros = new Scanner(System.in);
+        System.out.println("Ingrese mas numeros:");
+        System.out.println("¿Cuantos numeros quiere agregar?");
+        float preguntaPersonas = numeros.nextFloat();
+        for (int i = 0; i < preguntaPersonas; i++) {
+            Scanner nuevoNumero = new Scanner(System.in);
+            System.out.println("¿Que numero quiere agregar?");
+            int numero = nuevoNumero.nextInt();
+            this.HashNumero2.add(numero);
+        }
+    }
+
+    public void mismoNumero(){
+        boolean iguales = false;
+        for (int a : HashNumero1){
+            for (int b : HashNumero2){
+                if(a==b){
+                    iguales = true;
+                    System.out.println("Este es el numero que se repite: "+a);
+                }
+            }
+        }
+        if(iguales==false){
+            System.out.println("No hay numeros repetidos");
+        }
+    }
+
+    public void imprimir(){
+        for (int a : this.HashNumero){
+            System.out.println(a);
+        }
+    }
+}
---
+++ laboratorio/main9.5.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        SistemaRegistroDeNumeros a = new SistemaRegistro();
+        a.agregagarNumero();
+        a.agregagarOtroNumero();
+        a.mismoNumero();
+    }
+}
