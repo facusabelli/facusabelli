1b8fc194e82fac7005a6d7f497d58a67d8b2b4d5
facusabelli <73084328+facusabelli@users.noreply.github.com>
2021-05-12 11:10:47 -0300

    Add files via upload

---
+++ laboratorio/main3.5.java
@@ -0,0 +1,22 @@
+package com.company;
+import java.util.Scanner;
+
+import com.company.Producto;
+
+import java.util.ArrayList;
+
+
+public class main {
+
+    public static void main(String[] args){
+
+        SistemaDeRegistro s = new SistemaDeRegistro();
+        s.iniciarSistema()
+
+        ArrayList<Producto> productos = new ArrayList<>();
+        producto p1 = new producto(leche,50.5f);
+
+        
+    }
+}
+
---
+++ laboratorio/persona3.5.java
@@ -0,0 +1,30 @@
+package com.company;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Producto {
+
+    private String nombre;
+    private float precio;
+
+    public Producto(String nuevoNombre, float nuevoPrecio){
+        nombre = nuevoNombre;
+        precio = nuevoPrecio;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+ 
+    public float getPrecio(){
+        return precio;
+    }
+
+    public void setPrecio(float precio){
+        this.precio = precio;
+    }
+}
