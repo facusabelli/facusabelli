e44ca695db3d63892e501550df5d856344a4a09d
facusabelli <73084328+facusabelli@users.noreply.github.com>
2021-09-15 11:32:13 -0300

    Add files via upload

---
+++ laboratorio/Autor15.09.java
@@ -0,0 +1,15 @@
+public class Autor extends Persona{
+    private String nacionalidad;
+    public Autor(String nombre, String apellido, String fechaDeNacimiento, String nacionalidad){
+        super(nombre,apellido,fechaDeNacimiento);
+        this.nacionalidad=nacionalidad;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+}
